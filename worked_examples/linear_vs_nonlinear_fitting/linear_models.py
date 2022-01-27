2b6bfd7dca39fca94ae8ae5f7a67ef4fa098b0f8
sp8rks <34198848+sp8rks@users.noreply.github.com>
2022-01-27 09:43:43 -0700

    linear model with metrics and feature importance
    added feature importance and the classic models from best practices notebooks

--- worked_examples/linear_vs_nonlinear_fitting/linear_models.py
+++ worked_examples/linear_vs_nonlinear_fitting/linear_models.py
@@ -1,51 +1,192 @@
 from pandas import read_csv
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, Lasso
 from CBFV import composition
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import normalize
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot
+import pandas as pd
+from time import time
+from collections import OrderedDict
+from sklearn.dummy import DummyRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.svm import LinearSVR
+
 
 # %%
 
 # load the dataset and name it for cbfv
 df_train = read_csv('cp_train.csv')
+df_val = read_csv('cp_val.csv')
 df_test = read_csv('cp_test.csv')
 
 rename_dict = {'Cp': 'target'}
 df_train = df_train.rename(columns=rename_dict)
+df_val = df_val.rename(columns=rename_dict)
 df_test = df_test.rename(columns=rename_dict)
 
 #create cbfv
-X_train, y_train, formulae, skipped = composition.generate_features(df_train, elem_prop='mat2vec')
-X_test, y_test, formulae, skipped = composition.generate_features(df_test, elem_prop='mat2vec')
+X_train, y_train, formulae, skipped = composition.generate_features(df_train, elem_prop='oliynyk', drop_duplicates=False, extend_features=True, sum_feat=True)
+X_val, y_val, formulae, skipped = composition.generate_features(df_val, elem_prop='oliynyk', drop_duplicates=False, extend_features=True, sum_feat=True)
+X_test, y_test, formulae, skipped = composition.generate_features(df_test, elem_prop='oliynyk', drop_duplicates=False, extend_features=True, sum_feat=True)
 
 #scale and normalize
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
+X_val_scaled = scaler.transform(X_val)
 X_test_scaled = scaler.transform(X_test)
 X_train = normalize(X_train_scaled)
+X_val = normalize(X_val_scaled)
 X_test = normalize(X_test_scaled)
 
+# %%
 # define model with no hyperparameter tuning for now
-model = Ridge(alpha=1.0)
+model = Ridge(alpha=1.0, copy_X=True, fit_intercept=True, max_iter=None,
+                    normalize=False, random_state=None, solver='auto', tol=0.001)
 model.fit(X_train, y_train)
 
 #run model
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_val)
 
 #evaluate and collect metrics
-r2 = r2_score(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
-rmse_val = mean_squared_error(y_test, y_pred, squared=False)
+r2 = r2_score(y_val, y_pred)
+mae = mean_absolute_error(y_val, y_pred)
+rmse_val = mean_squared_error(y_val, y_pred, squared=False)
 
 #print metrics
-print(f'R^2 on test set is {r2:.2f}')
-print(f'MAE on test set is {mae:.2f}')
-print(f'RMSE on test set is {rmse_val:.2f}')
+print(f'R^2 on val set is {r2:.2f}')
+print(f'MAE on val set is {mae:.2f}')
+print(f'RMSE on val set is {rmse_val:.2f}')
+
+#calculate and report feature importance
+importance = model.coef_
+# for i,v in enumerate(importance):
+#     print('Feature: %0d, Score: %.5f' % (i,v))
+pyplot.bar([x for x in range(len(importance))], importance)
+pyplot.show()
+
+# %%
+# define model with no hyperparameter tuning for now
+model = Lasso(alpha=1.0)
+model.fit(X_train, y_train)
+
+#run model
+y_pred = model.predict(X_val)
+
+#evaluate and collect metrics
+r2 = r2_score(y_val, y_pred)
+mae = mean_absolute_error(y_val, y_pred)
+rmse_val = mean_squared_error(y_val, y_pred, squared=False)
+
+#print metrics
+print(f'R^2 on val set is {r2:.2f}')
+print(f'MAE on val set is {mae:.2f}')
+print(f'RMSE on val set is {rmse_val:.2f}')
+
+#calculate and report feature importance
+importance = model.coef_
+# for i,v in enumerate(importance):
+#     print('Feature: %0d, Score: %.5f' % (i,v))
+pyplot.bar([x for x in range(len(importance))], importance)
+pyplot.show()
+
+# %%
+#using the multi-model approach from notebooks
+def instantiate_model(model_name):
+    model = model_name()
+    return model
+
+def fit_model(model, X_train, y_train):
+    ti = time()
+    model = instantiate_model(model)
+    model.fit(X_train, y_train)
+    fit_time = time() - ti
+    return model, fit_time
+
+def evaluate_model(model, X, y_act):
+    y_pred = model.predict(X)
+    r2 = r2_score(y_act, y_pred)
+    mae = mean_absolute_error(y_act, y_pred)
+    rmse_val = mean_squared_error(y_act, y_pred, squared=False)
+    return r2, mae, rmse_val
+
+def fit_evaluate_model(model, model_name, X_train, y_train, X_val, y_act_val):
+    model, fit_time = fit_model(model, X_train, y_train)
+    r2_train, mae_train, rmse_train = evaluate_model(model, X_train, y_train)
+    r2_val, mae_val, rmse_val = evaluate_model(model, X_val, y_act_val)
+    result_dict = {
+        'model_name': model_name,
+        'model_name_pretty': type(model).__name__,
+        'model_params': model.get_params(),
+        'fit_time': fit_time,
+        'r2_train': r2_train,
+        'mae_train': mae_train,
+        'rmse_train': rmse_train,
+        'r2_val': r2_val,
+        'mae_val': mae_val,
+        'rmse_val': rmse_val}
+    return model, result_dict
+
+def append_result_df(df, result_dict):
+    df_result_appended = df.append(result_dict, ignore_index=True)
+    return df_result_appended
+
+def append_model_dict(dic, model_name, model):
+    dic[model_name] = model
+    return dic
+
+df_classics = pd.DataFrame(columns=['model_name',
+                                    'model_name_pretty',
+                                    'model_params',
+                                    'fit_time',
+                                    'r2_train',
+                                    'mae_train',
+                                    'rmse_train',
+                                    'r2_val',
+                                    'mae_val',
+                                    'rmse_val'])
+df_classics
+
+# Build a dictionary of model names
+classic_model_names = OrderedDict({
+    'dumr': DummyRegressor,
+    'rr': Ridge,
+    'abr': AdaBoostRegressor,
+    'gbr': GradientBoostingRegressor,
+    'rfr': RandomForestRegressor,
+    'etr': ExtraTreesRegressor,
+    'svr': SVR,
+    'lsvr': LinearSVR,
+    'knr': KNeighborsRegressor,
+})
+
+# Instantiate a dictionary to store the model objects
+classic_models = OrderedDict()
+
+# Keep track of elapsed time
+ti = time()
+
+# Loop through each model type, fit and predict, and evaluate and store results
+for model_name, model in classic_model_names.items():
+    print(f'Now fitting and evaluating model {model_name}: {model.__name__}')
+    model, result_dict = fit_evaluate_model(model, model_name, X_train, y_train, X_val, y_val)
+    df_classics = append_result_df(df_classics, result_dict)
+    classic_models = append_model_dict(classic_models, model_name, model)
+
+dt = time() - ti
+print(f'Finished fitting {len(classic_models)} models, total time: {dt:0.2f} s')
 
 
+# Sort in order of increasing validation r2 score
+df_classics = df_classics.sort_values('r2_val', ignore_index=True)
+print(df_classics)
 
 
 
